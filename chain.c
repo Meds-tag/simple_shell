4d4437e7aefefeb0e91ffb40335d634e58eba724
Meds-tag <123335821+Meds-tag@users.noreply.github.com>
2024-02-28 22:13:10 +0100

    Update chain.c

--- chain.c
+++ chain.c
@@ -2,13 +2,13 @@
 
 /**
  * azeis_chain - test if current char in buuffer is a chain delimeter
- * @info: the parameter struct
+ * @innfo: the parameter struct
  * @buf: the char buffer
  * @p: address of current position in buf
  *
  * Return: 1 if chain delimeter, 0 otherwise
  */
-int azeis_chain(info_t *info, char *buf, size_t *p)
+int azeis_chain(info_t *innfo, char *buf, size_t *p)
 {
 	size_t j = *p;
 
@@ -16,18 +16,18 @@ int azeis_chain(info_t *info, char *buf, size_t *p)
 	{
 		buf[j] = 0;
 		j++;
-		info->cmd_buf_type = CMD_OR;
+		innfo->cmd_buf_type = CMD_OR;
 	}
 	else if (buf[j] == '&' && buf[j + 1] == '&')
 	{
 		buf[j] = 0;
 		j++;
-		info->cmd_buf_type = CMD_AND;
+		innfo->cmd_buf_type = CMD_AND;
 	}
 	else if (buf[j] == ';') /* found end of this command */
 	{
 		buf[j] = 0; /* replace semicolon with null */
-		info->cmd_buf_type = CMD_CHAIN;
+		innfo->cmd_buf_type = CMD_CHAIN;
 	}
 	else
 		return (0);
@@ -37,7 +37,7 @@ int azeis_chain(info_t *info, char *buf, size_t *p)
 
 /**
  * azecheck_chain - checks we should continue chaining based on last status
- * @info: the parameter struct
+ * @innfo: the parameter struct
  * @buf: the char buffer
  * @p: address of currenrt position in buf
  * @i: starting position in buf
@@ -45,21 +45,21 @@ int azeis_chain(info_t *info, char *buf, size_t *p)
  *
  * Return: Void
  */
-void azecheck_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
+void azecheck_chain(info_t *innfo, char *buf, size_t *p, size_t i, size_t len)
 {
 	size_t j = *p;
 
-	if (info->cmd_buf_type == CMD_AND)
+	if (innfo->cmd_buf_type == CMD_AND)
 	{
-		if (info->status)
+		if (innfo->status)
 		{
 			buf[i] = 0;
 			j = len;
 		}
 	}
-	if (info->cmd_buf_type == CMD_OR)
+	if (innfo->cmd_buf_type == CMD_OR)
 	{
-		if (!info->status)
+		if (!innfo->status)
 		{
 			buf[i] = 0;
 			j = len;
@@ -71,40 +71,40 @@ void azecheck_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
 
 /**
  * azereplace_vars - replaces vars in the tokenized string
- * @info: the parameter struct
+ * @innfo: the parameter struct
  *
  * Return: 1 iiif replaced, 0 otherwise
  */
-int azereplace_vars(info_t *info)
+int azereplace_vars(info_t *innfo)
 {
 	int i = 0;
 	list_t *node;
 
-	for (i = 0; info->argv[i]; i++)
+	for (i = 0; innfo->argv[i]; i++)
 	{
-		if (info->argv[i][0] != '$' || !info->argv[i][1])
+		if (innfo->argv[i][0] != '$' || !innfo->argv[i][1])
 			continue;
 
-		if (!_azestrcmp(info->argv[i], "$?"))
+		if (!_azestrcmp(innfo->argv[i], "$?"))
 		{
-			azereplace_string(&(info->argv[i]),
-				_azestrdup(azeconvert_number(info->status, 10, 0)));
+			azereplace_string(&(innfo->argv[i]),
+				_azestrdup(azeconvert_number(innfo->status, 10, 0)));
 			continue;
 		}
-		if (!_azestrcmp(info->argv[i], "$$"))
+		if (!_azestrcmp(innfo->argv[i], "$$"))
 		{
-			azereplace_string(&(info->argv[i]),
+			azereplace_string(&(innfo->argv[i]),
 				_azestrdup(azeconvert_number(getpid(), 10, 0)));
 			continue;
 		}
-		node = azenode_starts_with(info->env, &info->argv[i][1], '=');
+		node = azenode_starts_with(innfo->env, &innfo->argv[i][1], '=');
 		if (node)
 		{
-			azereplace_string(&(info->argv[i]),
+			azereplace_string(&(innfo->argv[i]),
 				_azestrdup(_azestrchr(node->str, '=') + 1));
 			continue;
 		}
-		azereplace_string(&info->argv[i], _azestrdup(""));
+		azereplace_string(&innfo->argv[i], _azestrdup(""));
 
 	}
 	return (0);
@@ -112,11 +112,11 @@ int azereplace_vars(info_t *info)
 
 /**
  * azereplace_alias - replaces an aliases in the tokenized string
- * @info: the parameter struct
+ * @innfo: the parameter struct
  *
  * Return: 1 if repllaced, 0 otherwise
  */
-int azereplace_alias(info_t *info)
+int azereplace_alias(info_t *innfo)
 {
 	int i;
 	list_t *node;
@@ -124,31 +124,31 @@ int azereplace_alias(info_t *info)
 
 	for (i = 0; i < 10; i++)
 	{
-		node = azenode_starts_with(info->alias, info->argv[0], '=');
+		node = azenode_starts_with(innfo->alias, innfo->argv[0], '=');
 		if (!node)
 			return (0);
-		free(info->argv[0]);
+		free(innfo->argv[0]);
 		p = _azestrchr(node->str, '=');
 		if (!p)
 			return (0);
 		p = _azestrdup(p + 1);
 		if (!p)
 			return (0);
-		info->argv[0] = p;
+		innfo->argv[0] = p;
 	}
 	return (1);
 }
 
 /**
  * azereplace_string - replaces string
- * @old: address of old string
+ * @olld: address of old string
  * @new: new string
  *
  * Return: 1 ilf replaced, 0 otherwise
  */
-int azereplace_string(char **old, char *new)
+int azereplace_string(char **olld, char *new)
 {
-	free(*old);
-	*old = new;
+	free(*olld);
+	*olld = new;
 	return (1);
 }
