8ac129924dd1c1c7180b8c5d2ab3ad72db52950d
Meds-tag <123335821+Meds-tag@users.noreply.github.com>
2024-02-28 02:33:43 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,16 +2,18 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/wait.h> // Include sys/wait.h for wait function
 
 #define BUFFER_SIZE 1024
 #define DELIMITERS " \t\r\n\a"
 
-extern char **environ; // Access to the environment variables
+extern char **environ; /* Access to the environment variables */
 
 int main(void)
 {
     char *buffer;
     size_t bufsize = BUFFER_SIZE;
+    pid_t pid;
 
     buffer = (char *)malloc(bufsize * sizeof(char));
     if (buffer == NULL) {
@@ -19,14 +21,16 @@ int main(void)
         exit(1);
     }
 
+    char *token;
+    char *args[256]; // Maximum arguments count
+    int i;
+
     while (1) {
         printf("$ ");
         getline(&buffer, &bufsize, stdin);
         buffer[strcspn(buffer, "\n")] = 0;
 
-        char *token;
-        char *args[256]; // Maximum arguments count
-        int i = 0;
+        i = 0; // Initialize i before each iteration
 
         token = strtok(buffer, DELIMITERS);
         while (token != NULL) {
@@ -47,7 +51,7 @@ int main(void)
             continue; // Continue to prompt for input
         }
 
-        pid_t pid = fork();
+        pid = fork();
 
         if (pid == -1) {
             perror("fork");
@@ -55,8 +59,8 @@ int main(void)
             // Child process
             if (execve(args[0], args, NULL) == -1) {
                 perror(args[0]);
+                exit(EXIT_FAILURE); // Terminate child process if execve fails
             }
-            exit(EXIT_FAILURE);
         } else {
             // Parent process
             wait(NULL);
