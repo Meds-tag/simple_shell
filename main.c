e3a5bddf9423fad95db8a562f7165e73c71fc887
Meds-tag <123335821+Meds-tag@users.noreply.github.com>
2024-02-28 03:11:27 +0100

    Update main.c

--- main.c
+++ main.c
@@ -7,101 +7,68 @@
 #define BUFFER_SIZE 1024
 #define DELIMITERS " \t\r\n\a"
 
-void execute_command(char *args[]);
-void print_environment();
-void free_buffer(char *buffer);
+extern char **environ; /* Access to the environment variables */
 
 int main(void)
 {
     char *buffer;
     size_t bufsize = BUFFER_SIZE;
+    pid_t pid;
 
-    buffer = malloc(bufsize * sizeof(char));
-    if (buffer == NULL)
-    {
+    buffer = (char *)malloc(bufsize * sizeof(char));
+    if (buffer == NULL) {
         perror("Unable to allocate buffer");
         exit(1);
     }
 
-    while (1)
-    {
-        printf("$ ");
-        if (getline(&buffer, &bufsize, stdin) == -1)
-        {
-            free_buffer(buffer);
-            exit(EXIT_SUCCESS);
-        }
-        buffer[strcspn(buffer, "\n")] = '\0';
+    char *token;
+    char *args[256]; /* Maximum arguments count */
+    int i;
 
-        char *token;
-        char *args[256]; /* Maximum arguments count */
-        int i = 0;       /* Initialize i before each iteration */
+    i = 0; /* Initialize i before each iteration */
+
+    while (1) {
+        printf("$ ");
+        getline(&buffer, &bufsize, stdin);
+        buffer[strcspn(buffer, "\n")] = 0;
 
         token = strtok(buffer, DELIMITERS);
-        while (token != NULL)
-        {
+        while (token != NULL) {
             args[i++] = token;
             token = strtok(NULL, DELIMITERS);
         }
         args[i] = NULL; /* Mark the end of arguments with NULL */
 
-        if (strcmp(args[0], "exit") == 0)
-        {
-            free_buffer(buffer);
-            exit(EXIT_SUCCESS); /* Exit the shell */
+        if (strcmp(args[0], "exit") == 0) {
+            break; /* Exit the shell */
+        } else if (strcmp(args[0], "env") == 0) {
+            /* Print environment variables */
+            char **env = environ;
+            while (*env != NULL) {
+                printf("%s\n", *env);
+                env++;
+            }
+            continue; /* Continue to prompt for input */
         }
-        else if (strcmp(args[0], "env") == 0)
-        {
-            print_environment();
+
+        pid = fork();
+
+        if (pid == -1) {
+            perror("fork");
+        } else if (pid == 0) {
+            /* Child process */
+            if (execve(args[0], args, NULL) == -1) {
+                perror(args[0]);
+                exit(EXIT_FAILURE); /* Terminate child process if execve fails */
+            }
+        } else {
+            /* Parent process */
+            wait(NULL);
         }
-        else
-        {
-            execute_command(args);
-        }
+
+        i = 0; /* Reset i for next iteration */
     }
 
-    free_buffer(buffer);
+    free(buffer);
     return 0;
-}
-
-void execute_command(char *args[])
-{
-    pid_t pid = fork();
-
-    if (pid == -1)
-    {
-        perror("fork");
-    }
-    else if (pid == 0)
-    {
-        /* Child process */
-        if (execve(args[0], args, NULL) == -1)
-        {
-            perror(args[0]);
-            exit(EXIT_FAILURE); /* Terminate child process if execve fails */
-        }
-    }
-    else
-    {
-        /* Parent process */
-        wait(NULL);
-    }
-}
-
-void print_environment()
-{
-    char **env = environ;
-    while (*env != NULL)
-    {
-        printf("%s\n", *env);
-        env++;
-    }
-}
-
-void free_buffer(char *buffer)
-{
-    if (buffer != NULL)
-    {
-        free(buffer);
-    }
 }
