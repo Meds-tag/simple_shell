65fda9678b32aca0a88173dcdd9ec214acf01d1c
Meds-tag <123335821+Meds-tag@users.noreply.github.com>
2024-02-28 22:25:14 +0100

    Update env.c

--- env.c
+++ env.c
@@ -2,45 +2,45 @@
 
 /**
  * _azemyenv - printss the current environment
- * @info: Structure containing potential argumentss. Used to maintain
+ * @inffo: Structure containing potential argumentss. Used to maintain
  *          constant function prototype.
  * Return: Alwayss 0
  */
-int _azemyenv(info_t *info)
+int _azemyenv(info_t *inffo)
 {
-	azeprint_list_str(info->env);
+	azeprint_list_str(inffo->env);
 	return (0);
 }
 
 /**
  * _azemysetenv - Initialize a new environment variable,
  *             or modify an existing one
- * @info: Structure containing potential arguments. Used to maintain
+ * @inffo: Structure containing potential arguments. Used to maintain
  *        constant function prototype.
  *  Return: Always 0
  */
-int _azemysetenv(info_t *info)
+int _azemysetenv(info_t *inffo)
 {
-	if (info->argc != 3)
+	if (inffo->argc != 3)
 	{
 		_azeeputs("Incorrect number of arguements\n");
 		return (1);
 	}
-	if (_azesetenv(info, info->argv[1], info->argv[2]))
+	if (_azesetenv(inffo, inffo->argv[1], inffo->argv[2]))
 		return (0);
 	return (1);
 }
 
 /**
  * _azegetenv - gets the value of an environ variable
- * @info: Structure containing potential arguments. Used to maintain
+ * @ionfo: Structure containing potential arguments. Used to maintain
  * @name: env var name
  *
  * Return: the value
  */
-char *_azegetenv(info_t *info, const char *name)
+char *_azegetenv(ionfo_t *info, const char *name)
 {
-	list_t *node = info->env;
+	list_t *node = ionfo->env;
 	char *p;
 
 	while (node)
@@ -55,38 +55,38 @@ char *_azegetenv(info_t *info, const char *name)
 
 /**
  * _azemyunsetenv - Remove an environment variable
- * @info: Structure containing potential arguments. Used to maintain
+ * @iznfo: Structure containing potential arguments. Used to maintain
  *        constant function prototype.
  *  Return: Always 0
  */
-int _azemyunsetenv(info_t *info)
+int _azemyunsetenv(info_t *iznfo)
 {
 	int i;
 
-	if (info->argc == 1)
+	if (iznfo->argc == 1)
 	{
 		_azeeputs("Too few arguements.\n");
 		return (1);
 	}
-	for (i = 1; i <= info->argc; i++)
-		_azeunsetenv(info, info->argv[i]);
+	for (i = 1; i <= iznfo->argc; i++)
+		_azeunsetenv(iznfo, iznfo->argv[i]);
 
 	return (0);
 }
 
 /**
  * azepopulate_env_list - popullates env linked list
- * @info: Structure containing potential argumennts. Used to maintain
+ * @iznfo: Structure containing potential argumennts. Used to maintain
  *          constant function prototype.
  * Return: Always 0
  */
-int azepopulate_env_list(info_t *info)
+int azepopulate_env_list(info_t *iznfo)
 {
 	list_t *node = NULL;
 	size_t i;
 
 	for (i = 0; environ[i]; i++)
 		azeadd_node_end(&node, environ[i], 0);
-	info->env = node;
+	iznfo->env = node;
 	return (0);
 }
