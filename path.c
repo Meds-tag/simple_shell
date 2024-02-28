099fbb5d906e67d66b99d309045d9b5006c3507e
Meds-tag <123335821+Meds-tag@users.noreply.github.com>
2024-02-28 23:16:27 +0100

    Update path.c

--- path.c
+++ path.c
@@ -2,17 +2,17 @@
 
 /**
  * azeis_cmd - determines if a file is an executable command
- * @info: the info struct
- * @path: path to the file
+ * @zinfo: the info struct
+ * @apath: path to the file
  *
  * Return: 1 if true, 0 otherwise
  */
-int azeis_cmd(info_t *info, char *path)
+int azeis_cmd(info_t *zinfo, char *apath)
 {
 	struct stat st;
 
-	(void)info;
-	if (!path || stat(path, &st))
+	(void)zinfo;
+	if (!apath || stat(apath, &st))
 		return (0);
 
 	if (st.st_mode & S_IFREG)
@@ -23,64 +23,64 @@ int azeis_cmd(info_t *info, char *path)
 }
 
 /**
- * azedup_chars - duplicates characters
- * @pathstr: the PATH string
- * @start: starting index
- * @stop: stopping index
- *
- * Return: pointer to new buffer
- */
-char *azedup_chars(char *pathstr, int start, int stop)
-{
-	static char buf[1024];
-	int i = 0, k = 0;
-
-	for (k = 0, i = start; i < stop; i++)
-		if (pathstr[i] != ':')
-			buf[k++] = pathstr[i];
-	buf[k] = 0;
-	return (buf);
-}
-
-/**
  * azefind_path - finds this cmd in the PATH string
- * @info: the info struct
- * @pathstr: the PATH string
- * @cmd: the cmd to find
+ * @winfo: the info struct
+ * @patstring: the PATH string
+ * @acmd: the cmd to find
  *
  * Return: full path of cmd if found or NULL
  */
-char *azefind_path(info_t *info, char *pathstr, char *cmd)
+char *azefind_path(info_t *winfo, char *patstring, char *acmd)
 {
 	int i = 0, curr_pos = 0;
 	char *path;
 
-	if (!pathstr)
+	if (!patstring)
 		return (NULL);
-	if ((_azestrlen(cmd) > 2) && azestarts_with(cmd, "./"))
+	if ((_azestrlen(acmd) > 2) && azestarts_with(acmd, "./"))
 	{
-		if (azeis_cmd(info, cmd))
-			return (cmd);
+		if (azeis_cmd(winfo, acmd))
+			return (acmd);
 	}
 	while (1)
 	{
-		if (!pathstr[i] || pathstr[i] == ':')
+		if (!patstring[i] || patstring[i] == ':')
 		{
-			path = azedup_chars(pathstr, curr_pos, i);
+			path = azedup_chars(patstring, curr_pos, i);
 			if (!*path)
-				_azestrcat(path, cmd);
+				_azestrcat(path, acmd);
 			else
 			{
 				_azestrcat(path, "/");
-				_azestrcat(path, cmd);
+				_azestrcat(path, acmd);
 			}
-			if (azeis_cmd(info, path))
+			if (azeis_cmd(winfo, path))
 				return (path);
-			if (!pathstr[i])
+			if (!patstring[i])
 				break;
 			curr_pos = i;
 		}
 		i++;
 	}
 	return (NULL);
+}
+
+/**
+ * azedup_chars - duplicates characters
+ * @patstring: the PATH string
+ * @start: starting index
+ * @stop: stopping index
+ *
+ * Return: pointer to new buffer
+ */
+char *azedup_chars(char *patstring, int start, int stop)
+{
+	static char buf[1024];
+	int i = 0, k = 0;
+
+	for (k = 0, i = start; i < stop; i++)
+		if (patstring[i] != ':')
+			buf[k++] = patstring[i];
+	buf[k] = 0;
+	return (buf);
 }
