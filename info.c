d202a6f3fa3b82635c83dfeb076f32ef8cee187b
Meds-tag <123335821+Meds-tag@users.noreply.github.com>
2024-02-28 20:54:19 +0100

    Update info.c

--- info.c
+++ info.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 
 /**
- * clear_info - initializes info_t struct
+ * azeclear_info - initializes info_t struct
  * @info: struct address
  */
-void clear_info(info_t *info)
+void azeclear_info(info_t *info)
 {
 	info->arg = NULL;
 	info->argv = NULL;
@@ -13,25 +13,25 @@ void clear_info(info_t *info)
 }
 
 /**
- * set_info - initializes info_t struct
+ * azeset_info - initializes info_t struct
  * @info: struct address
  * @av: argument vector
  */
-void set_info(info_t *info, char **av)
+void azeset_info(info_t *info, char **av)
 {
 	int i = 0;
 
 	info->fname = av[0];
 	if (info->arg)
 	{
-		info->argv = strtow(info->arg, " \t");
+		info->argv = azestrtow(info->arg, " \t");
 		if (!info->argv)
 		{
 
 			info->argv = malloc(sizeof(char *) * 2);
 			if (info->argv)
 			{
-				info->argv[0] = _strdup(info->arg);
+				info->argv[0] = _azestrdup(info->arg);
 				info->argv[1] = NULL;
 			}
 		}
@@ -39,19 +39,19 @@ void set_info(info_t *info, char **av)
 			;
 		info->argc = i;
 
-		replace_alias(info);
-		replace_vars(info);
+		azereplace_alias(info);
+		azereplace_vars(info);
 	}
 }
 
 /**
- * free_info - frees info_t struct fields
+ * azefree_info - frees info_t struct fields
  * @info: struct address
  * @all: true if freeing all fields
  */
-void free_info(info_t *info, int all)
+void azefree_info(info_t *info, int all)
 {
-	ffree(info->argv);
+	azeffree(info->argv);
 	info->argv = NULL;
 	info->path = NULL;
 	if (all)
@@ -59,16 +59,16 @@ void free_info(info_t *info, int all)
 		if (!info->cmd_buf)
 			free(info->arg);
 		if (info->env)
-			free_list(&(info->env));
+			azefree_list(&(info->env));
 		if (info->history)
-			free_list(&(info->history));
+			azefree_list(&(info->history));
 		if (info->alias)
-			free_list(&(info->alias));
-		ffree(info->environ);
+			azefree_list(&(info->alias));
+		azeffree(info->environ);
 			info->environ = NULL;
-		bfree((void **)info->cmd_buf);
+		azebfree((void **)info->cmd_buf);
 		if (info->readfd > 2)
 			close(info->readfd);
-		_putchar(BUF_FLUSH);
+		_azeputchar(BUF_FLUSH);
 	}
 }
