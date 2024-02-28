c91711ddbec866e4ac7312536141889e5e1ea9e9
Meds-tag <123335821+Meds-tag@users.noreply.github.com>
2024-02-28 22:43:21 +0100

    Update hsh.c

--- hsh.c
+++ hsh.c
@@ -2,57 +2,99 @@
 
 /**
  * hsh - main shell loop
- * @info: the parameter & return info struct
- * @av: the argument vector from main()
+ * @nfo: the parameter & return info struct
+ * @iav: the argument vector from main()
  *
  * Return: 0 on success, 1 on error, or error code
  */
-int hsh(info_t *info, char **av)
+int hsh(info_t *nfo, char **iav)
 {
 	ssize_t r = 0;
 	int builtin_ret = 0;
 
 	while (r != -1 && builtin_ret != -2)
 	{
-		azeclear_info(info);
+		azeclear_info(nfo);
 		if (azeinteractive(info))
 			_azeputs("$ ");
 		_azeeputchar(BUF_FLUSH);
-		r = azeget_input(info);
+		r = azeget_input(nfo);
 		if (r != -1)
 		{
-			azeset_info(info, av);
-			builtin_ret = azefind_builtin(info);
+			azeset_info(nfo, iav);
+			builtin_ret = azefind_builtin(nfo);
 			if (builtin_ret == -1)
-				azefind_cmd(info);
+				azefind_cmd(nfo);
 		}
-		else if (azeinteractive(info))
+		else if (azeinteractive(nfo))
 			_azeputchar('\n');
-		azefree_info(info, 0);
+		azefree_info(nfo, 0);
 	}
-	azewrite_history(info);
-	azefree_info(info, 1);
-	if (!azeinteractive(info) && info->status)
-		exit(info->status);
+	azewrite_history(nfo);
+	azefree_info(nfo, 1);
+	if (!azeinteractive(nfo) && nfo->status)
+		exit(nfo->status);
 	if (builtin_ret == -2)
 	{
-		if (info->err_num == -1)
-			exit(info->status);
-		exit(info->err_num);
+		if (nfo->err_num == -1)
+			exit(nfo->status);
+		exit(nfo->err_num);
 	}
 	return (builtin_ret);
 }
 
 /**
- * azefind_builtin - finds a builtin command
- * @info: the parameter & return info struct
+ * azefind_cmd - finds a command in PATH
+ * @nfo: the parameter & return info struct
  *
- * Return: -1 if builtin not found,
+ * Return: void
+ */
+void azefind_cmd(info_t *nfo)
+{
+	char *path = NULL;
+	int i, k;
+
+	nfo->path = nfo->argv[0];
+	if (nfo->linecount_flag == 1)
+	{
+		nfo->line_count++;
+		nfo->linecount_flag = 0;
+	}
+	for (i = 0, k = 0; nfo->arg[i]; i++)
+		if (!azeis_delim(info->arg[i], " \t\n"))
+			k++;
+	if (!k)
+		return;
+
+	path = azefind_path(nfo, _azegetenv(info, "PATH="), nfo->argv[0]);
+	if (path)
+	{
+		nfo->path = path;
+		azefork_cmd(nfo);
+	}
+	else
+	{
+		if ((azeinteractive(nfo) || _azegetenv(nfo, "PATH=")
+			|| nfo->argv[0][0] == '/') && azeis_cmd(nfo, nfo->argv[0]))
+			azefork_cmd(nfo);
+		else if (*(nfo->arg) != '\n')
+		{
+			nfo->status = 127;
+			azeprint_error(nfo, "not found\n");
+		}
+	}
+}
+
+/**
+ * azefind_builtin - finds a builtin command
+ * @innfo: the parameter & return info struct
+ *
+ * Return: -1 if built in not found,
  *			0 if builtin executed successfully,
  *			1 if builtin found but not successful,
  *			-2 if builtin signals exit()
  */
-int azefind_builtin(info_t *info)
+int azefind_builtin(info_t *innfo)
 {
 	int i, built_in_ret = -1;
 	builtin_table builtintbl[] = {
@@ -68,64 +110,23 @@ int azefind_builtin(info_t *info)
 	};
 
 	for (i = 0; builtintbl[i].type; i++)
-		if (_azestrcmp(info->argv[0], builtintbl[i].type) == 0)
+		if (_azestrcmp(innfo->argv[0], builtintbl[i].type) == 0)
 		{
-			info->line_count++;
-			built_in_ret = builtintbl[i].func(info);
+			innfo->line_count++;
+			built_in_ret = builtintbl[i].func(innfo);
 			break;
 		}
 	return (built_in_ret);
 }
 
-/**
- * azefind_cmd - finds a command in PATH
- * @info: the parameter & return info struct
- *
- * Return: void
- */
-void azefind_cmd(info_t *info)
-{
-	char *path = NULL;
-	int i, k;
-
-	info->path = info->argv[0];
-	if (info->linecount_flag == 1)
-	{
-		info->line_count++;
-		info->linecount_flag = 0;
-	}
-	for (i = 0, k = 0; info->arg[i]; i++)
-		if (!azeis_delim(info->arg[i], " \t\n"))
-			k++;
-	if (!k)
-		return;
-
-	path = azefind_path(info, _azegetenv(info, "PATH="), info->argv[0]);
-	if (path)
-	{
-		info->path = path;
-		azefork_cmd(info);
-	}
-	else
-	{
-		if ((azeinteractive(info) || _azegetenv(info, "PATH=")
-			|| info->argv[0][0] == '/') && azeis_cmd(info, info->argv[0]))
-			azefork_cmd(info);
-		else if (*(info->arg) != '\n')
-		{
-			info->status = 127;
-			azeprint_error(info, "not found\n");
-		}
-	}
-}
 
 /**
  * azefork_cmd - forks a an exec thread to run cmd
- * @info: the parameter & return info struct
+ * @nfo: the parameter & return info struct
  *
  * Return: void
  */
-void azefork_cmd(info_t *info)
+void azefork_cmd(info_t *nfo)
 {
 	pid_t child_pid;
 
@@ -138,9 +139,9 @@ void azefork_cmd(info_t *info)
 	}
 	if (child_pid == 0)
 	{
-		if (execve(info->path, info->argv, azeget_environ(info)) == -1)
+		if (execve(nfo->path, nfo->argv, azeget_environ(nfo)) == -1)
 		{
-			azefree_info(info, 1);
+			azefree_info(nfo, 1);
 			if (errno == EACCES)
 				exit(126);
 			exit(1);
@@ -149,12 +150,12 @@ void azefork_cmd(info_t *info)
 	}
 	else
 	{
-		wait(&(info->status));
+		wait(&(nfo->status));
 		if (WIFEXITED(info->status))
 		{
-			info->status = WEXITSTATUS(info->status);
-			if (info->status == 126)
-				azeprint_error(info, "Permission denied\n");
+			info->status = WEXITSTATUS(nfo->status);
+			if (nfo->status == 126)
+				azeprint_error(nfo, "Permission denied\n");
 		}
 	}
 }
