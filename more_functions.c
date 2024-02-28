c27a8f0b51ea8a3c18d254b1263ffefd2b6f707d
Meds-tag <123335821+Meds-tag@users.noreply.github.com>
2024-02-28 23:06:01 +0100

    Update more_functions.c

--- more_functions.c
+++ more_functions.c
@@ -2,38 +2,38 @@
 
 /**
  * azeinteractive - returns true if shell is interactive mode
- * @info: struct address
- *
+ * @nfo: struct address
+ *olo
  * Return: 1 if interactive mode, 0 otherwise
  */
-int azeinteractive(info_t *info)
+int azeinteractive(info_t *nfo)
 {
-	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+	return (isatty(STDIN_FILENO) && nfo->readfd <= 2);
 }
 
 /**
  * azeis_delim - checks if character is a delimeter
- * @c: the char to check
- * @delim: the delimeter string
+ * @r: the char to check
+ * @elim: the delimeter string
  * Return: 1 if true, 0 if false
  */
-int azeis_delim(char c, char *delim)
+int azeis_delim(char r, char *elim)
 {
-	while (*delim)
-		if (*delim++ == c)
+	while (*elim)
+		if (*elim++ == r)
 			return (1);
 	return (0);
 }
 
 /**
  *_azeisalpha - checks for alphabetic character
- *@c: The character to input
+ *@cc: The character to input
  *Return: 1 if c is alphabetic, 0 otherwise
  */
 
-int _azeisalpha(int c)
+int _azeisalpha(int cc)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if ((cc >= 'a' && cc <= 'z') || (cc >= 'A' && cc <= 'Z'))
 		return (1);
 	else
 		return (0);
@@ -41,25 +41,25 @@ int _azeisalpha(int c)
 
 /**
  *_azeatoi - converts a string to an integer
- *@s: the string to be converted
+ *@cs: the string to be converted
  *Return: 0 if no numbers in string, converted number otherwise
  */
 
-int _azeatoi(char *s)
+int _azeatoi(char *cs)
 {
 	int i, sign = 1, flag = 0, output;
 	unsigned int result = 0;
 
 	for (i = 0;  s[i] != '\0' && flag != 2; i++)
 	{
-		if (s[i] == '-')
+		if (cs[i] == '-')
 			sign *= -1;
 
-		if (s[i] >= '0' && s[i] <= '9')
+		if (cs[i] >= '0' && cs[i] <= '9')
 		{
 			flag = 1;
 			result *= 10;
-			result += (s[i] - '0');
+			result += (cs[i] - '0');
 		}
 		else if (flag == 1)
 			flag = 2;
