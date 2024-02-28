49a73fd187d41c5b77c3dc74649d159092c4ec16
Meds-tag <123335821+Meds-tag@users.noreply.github.com>
2024-02-28 21:48:41 +0100

    Update chain.c

--- chain.c
+++ chain.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * azeis_chain - test if current char in buffer is a chain delimeter
+ * azeis_chain - test if current char in buuffer is a chain delimeter
  * @info: the parameter struct
  * @buf: the char buffer
  * @p: address of current position in buf
@@ -39,7 +39,7 @@ int azeis_chain(info_t *info, char *buf, size_t *p)
  * azecheck_chain - checks we should continue chaining based on last status
  * @info: the parameter struct
  * @buf: the char buffer
- * @p: address of current position in buf
+ * @p: address of currenrt position in buf
  * @i: starting position in buf
  * @len: length of buf
  *
@@ -70,39 +70,10 @@ void azecheck_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
 }
 
 /**
- * azereplace_alias - replaces an aliases in the tokenized string
- * @info: the parameter struct
- *
- * Return: 1 if replaced, 0 otherwise
- */
-int azereplace_alias(info_t *info)
-{
-	int i;
-	list_t *node;
-	char *p;
-
-	for (i = 0; i < 10; i++)
-	{
-		node = azenode_starts_with(info->alias, info->argv[0], '=');
-		if (!node)
-			return (0);
-		free(info->argv[0]);
-		p = _azestrchr(node->str, '=');
-		if (!p)
-			return (0);
-		p = _azestrdup(p + 1);
-		if (!p)
-			return (0);
-		info->argv[0] = p;
-	}
-	return (1);
-}
-
-/**
  * azereplace_vars - replaces vars in the tokenized string
  * @info: the parameter struct
  *
- * Return: 1 if replaced, 0 otherwise
+ * Return: 1 iiif replaced, 0 otherwise
  */
 int azereplace_vars(info_t *info)
 {
@@ -140,11 +111,40 @@ int azereplace_vars(info_t *info)
 }
 
 /**
+ * azereplace_alias - replaces an aliases in the tokenized string
+ * @info: the parameter struct
+ *
+ * Return: 1 if repllaced, 0 otherwise
+ */
+int azereplace_alias(info_t *info)
+{
+	int i;
+	list_t *node;
+	char *p;
+
+	for (i = 0; i < 10; i++)
+	{
+		node = azenode_starts_with(info->alias, info->argv[0], '=');
+		if (!node)
+			return (0);
+		free(info->argv[0]);
+		p = _azestrchr(node->str, '=');
+		if (!p)
+			return (0);
+		p = _azestrdup(p + 1);
+		if (!p)
+			return (0);
+		info->argv[0] = p;
+	}
+	return (1);
+}
+
+/**
  * azereplace_string - replaces string
  * @old: address of old string
  * @new: new string
  *
- * Return: 1 if replaced, 0 otherwise
+ * Return: 1 ilf replaced, 0 otherwise
  */
 int azereplace_string(char **old, char *new)
 {
