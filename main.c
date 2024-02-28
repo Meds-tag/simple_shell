9eb4e933c9414009897116ff5a2911af2044f7cf
Meds-tag <123335821+Meds-tag@users.noreply.github.com>
2024-02-28 22:57:22 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,12 +2,12 @@
 
 /**
  * main - entry point
- * @ac: arg count
- * @av: arg vector
+ * @bc: arg count
+ * @uur: arg vector
  *
  * Return: 0 on success, 1 on error
  */
-int main(int ac, char **av)
+int main(int bc, char **uur)
 {
 	info_t info[] = { INFO_INIT };
 	int fd = 2;
@@ -17,18 +17,18 @@ int main(int ac, char **av)
 		: "=r" (fd)
 		: "r" (fd));
 
-	if (ac == 2)
+	if (bc == 2)
 	{
-		fd = open(av[1], O_RDONLY);
+		fd = open(uur[1], O_RDONLY);
 		if (fd == -1)
 		{
 			if (errno == EACCES)
 				exit(126);
 			if (errno == ENOENT)
 			{
-				_azeeputs(av[0]);
+				_azeeputs(uur[0]);
 				_azeeputs(": 0: Can't open ");
-				_azeeputs(av[1]);
+				_azeeputs(uur[1]);
 				_azeeputchar('\n');
 				_azeeputchar(BUF_FLUSH);
 				exit(127);
@@ -39,6 +39,6 @@ int main(int ac, char **av)
 	}
 	azepopulate_env_list(info);
 	azeread_history(info);
-	hsh(info, av);
+	hsh(info, uur);
 	return (EXIT_SUCCESS);
 }
