08b77e19f2c22eb7d5782bbeb2bcffe48954dc46
Meds-tag <123335821+Meds-tag@users.noreply.github.com>
2024-02-28 23:29:48 +0100

    Update string_functions3.c

--- string_functions3.c
+++ string_functions3.c
@@ -2,28 +2,28 @@
 
 /**
  **_azestrncpy - copies a string
- *@dest: the destination string to be copied to
- *@src: the source string
- *@n: the amount of characters to be copied
+ *@chestt: the chesttination string to be copied to
+ *@sourcc: the source string
+ *@qn: the amount of characters to be copied
  *Return: the concatenated string
  */
-char *_azestrncpy(char *dest, char *src, int n)
+char *_azestrncpy(char *chestt, char *sourcc, int qn)
 {
 	int i, j;
-	char *s = dest;
+	char *s = chestt;
 
 	i = 0;
-	while (src[i] != '\0' && i < n - 1)
+	while (sourcc[i] != '\0' && i < qn - 1)
 	{
-		dest[i] = src[i];
+		chestt[i] = sourcc[i];
 		i++;
 	}
-	if (i < n)
+	if (i < qn)
 	{
 		j = i;
-		while (j < n)
+		while (j < qn)
 		{
-			dest[j] = '\0';
+			chestt[j] = '\0';
 			j++;
 		}
 	}
@@ -31,44 +31,44 @@ char *_azestrncpy(char *dest, char *src, int n)
 }
 
 /**
- **_azestrncat - concatenates two strings
- *@dest: the first string
- *@src: the second string
- *@n: the amount of bytes to be maximally used
- *Return: the concatenated string
+ **_azestrchr - locates a character in a string
+ *@qs: the string to be parsed
+ *@qc: the character to look for
+ *Return: (s) a pointer to the memory area s
  */
-char *_azestrncat(char *dest, char *src, int n)
+char *_azestrchr(char *qs, char qc)
 {
-	int i, j;
-	char *s = dest;
+	do {
+		if (*qs == qc)
+			return (qs);
+	} while (*qs++ != '\0');
 
-	i = 0;
-	j = 0;
-	while (dest[i] != '\0')
-		i++;
-	while (src[j] != '\0' && j < n)
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	if (j < n)
-		dest[i] = '\0';
-	return (s);
+	return (NULL);
 }
 
 /**
- **_azestrchr - locates a character in a string
- *@s: the string to be parsed
- *@c: the character to look for
- *Return: (s) a pointer to the memory area s
+ **_azestrncat - concatenates two strings
+ *@chestt: the first string
+ *@sourcc: the second string
+ *@qn: the amount of bytes to be maximally used
+ *Return: the concatenated string
  */
-char *_azestrchr(char *s, char c)
+char *_azestrncat(char *chestt, char *sourcc, int qn)
 {
-	do {
-		if (*s == c)
-			return (s);
-	} while (*s++ != '\0');
+	int i, j;
+	char *s = chestt;
 
-	return (NULL);
+	i = 0;
+	j = 0;
+	while (chestt[i] != '\0')
+		i++;
+	while (sourcc[j] != '\0' && j < qn)
+	{
+		chestt[i] = sourcc[j];
+		i++;
+		j++;
+	}
+	if (j < qn)
+		chestt[i] = '\0';
+	return (s);
 }
