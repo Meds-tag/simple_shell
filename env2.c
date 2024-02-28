aee838cec407446d866448175cf9d2fbc5a5d981
Meds-tag <123335821+Meds-tag@users.noreply.github.com>
2024-02-28 22:26:33 +0100

    Update env2.c

--- env2.c
+++ env2.c
@@ -2,31 +2,31 @@
 
 /**
  * azeget_environ - returns the strinng array copy of our environ
- * @info: Structure containing potential argumeints. Used to maintain
+ * @iznfo: Structure containing potential argumeints. Used to maintain
  *          constant function prototype.
  * Return: Always 0
  */
-char **azeget_environ(info_t *info)
+char **azeget_environ(info_t *iznfo)
 {
-	if (!info->environ || info->env_changed)
+	if (!iznfo->environ || iznfo->env_changed)
 	{
-		info->environ = azelist_to_strings(info->env);
-		info->env_changed = 0;
+		iznfo->environ = azelist_to_strings(iznfo->env);
+		iznfo->env_changed = 0;
 	}
 
-	return (info->environ);
+	return (iznfo->environ);
 }
 
 /**
  * _azesetenv - Initialize a new environment variable,
  *             or modify an existing one
- * @info: Structure containing potential arguments. Used to maintain
+ * @iznfo: Structure containing potential arguments. Used to maintain
  *        constant function prototype.
  * @var: the string env var propperty
  * @value: the string env var value
  *  Return: Alwayys 0
  */
-int _azesetenv(info_t *info, char *var, char *value)
+int _azesetenv(info_t *iznfo, char *var, char *value)
 {
 	char *buf = NULL;
 	list_t *node;
@@ -41,7 +41,7 @@ int _azesetenv(info_t *info, char *var, char *value)
 	_azestrcpy(buf, var);
 	_azestrcat(buf, "=");
 	_azestrcat(buf, value);
-	node = info->env;
+	node = iznfo->env;
 	while (node)
 	{
 		p = azestarts_with(node->str, var);
@@ -49,25 +49,25 @@ int _azesetenv(info_t *info, char *var, char *value)
 		{
 			free(node->str);
 			node->str = buf;
-			info->env_changed = 1;
+			iznfo->env_changed = 1;
 			return (0);
 		}
 		node = node->next;
 	}
-	azeadd_node_end(&(info->env), buf, 0);
+	azeadd_node_end(&(iznfo->env), buf, 0);
 	free(buf);
-	info->env_changed = 1;
+	iznfo->env_changed = 1;
 	return (0);
 }
 
 /**
  * _azeunsetenv - Remove  an environment variable
- * @info: Structure containing potential arguments. Used to maintain
+ * @iznfo: Structure containing potential arguments. Used to maintain
  *        constant  function prototype.
  *  Return: 1 on d elete, 0 otherwise
  * @var: the string env var propertyy
  */
-int _azeunsetenv(info_t *info, char *var)
+int _azeunsetenv(info_t *iznfo, char *var)
 {
 	list_t *node = info->env;
 	size_t i = 0;
@@ -81,13 +81,13 @@ int _azeunsetenv(info_t *info, char *var)
 		p = azestarts_with(node->str, var);
 		if (p && *p == '=')
 		{
-			info->env_changed = azedelete_node_at_index(&(info->env), i);
+			iznfo->env_changed = azedelete_node_at_index(&(iznfo->env), i);
 			i = 0;
-			node = info->env;
+			node = iznfo->env;
 			continue;
 		}
 		node = node->next;
 		i++;
 	}
-	return (info->env_changed);
+	return (iznfo->env_changed);
 }
